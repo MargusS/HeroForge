30a3c0ef2241ab9d06497ef101427a46e28616bd
agustin.marani <agustin.marani@bitboxonline.com>
2025-07-15 11:58:00 +0100

    fix: Update padding property in TaskListWrapper for consistent layout

--- BitLogs/src/frontend/components/taskListWrapper/TaskListWrapper.jsx
+++ BitLogs/src/frontend/components/taskListWrapper/TaskListWrapper.jsx
@@ -8,16 +8,14 @@ const TaskListWrapper = ({ groupedTasks }) => {
   const { loading, tasks, canSearch, canExport } = useSearchContext();
   if (loading) {
     return (
-      <Box padding="space.300">
+      <Box paddingBlock="space.300">
         <Text>Procesando lotes... Esto puede tardar unos segundos</Text>
       </Box>
     );
   }
 
   if (!loading && tasks.length === 0 && !canExport && canSearch) {
-    return (
-      <WorklogsNotFound />
-    );
+    return <WorklogsNotFound />;
   }
 
   return <TaskList groupedTasks={groupedTasks} />;
